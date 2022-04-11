5569034a3f848319858e37d45bd215e398e728dc
Markus Schmidt <markus@deuso.de>
2022-04-12 00:20:47 +0200

    Referee: hide superfluous brackets

--- agt-o/src/components/RefereeTeam.js
+++ agt-o/src/components/RefereeTeam.js
@@ -9,7 +9,7 @@ export default function RefereeTeam(props) {
   const color = Stati[data.state || 'default'].color;
 
   const points = parseFloat(data.points) ? (parseFloat(data.points || 0) * parseFloat(discipline.multiplier || 1)).toFixed(2) : '--';
-  const pointsdetails = parseFloat(data.points) ? data.points + ' ' + (discipline.type === 'seconds' ? 'Sek.' : 'Fehler') : '';
+  const pointsdetails = parseFloat(data.points) ? '(' + data.points + ' ' + (discipline.type === 'seconds' ? 'Sek.' : 'Fehler') + ')' : '';
   const penalty = parseFloat(data.penalty) ? Math.round(parseFloat(data.penalty) * parseFloat(discipline.faults || 1)) : '--';
   const penaltydetails = parseFloat(data.penalty) ? data.penalty + ' Fehler' : '';
 
@@ -22,7 +22,7 @@ export default function RefereeTeam(props) {
       <span className="title"><span className="title">{ data.name }</span><span className="state" style={ { background: color } }><Icon>status</Icon><span className="status">{ status }</span></span></span>
       <span className="pointsi"><Icon>medal</Icon></span>
       <span className="pointsd">Punkte</span>
-      <span className="points">{ points } <span className="small">({ pointsdetails })</span> { data.referee === user.id ? <Icon>user</Icon> : null }</span>
+      <span className="points">{ points } <span className="small">{ pointsdetails }</span> { data.referee === user.id ? <Icon>user</Icon> : null }</span>
       {
         parseFloat(data.penalty || 0) ? <>
           <span className="penaltyi"><Icon>referee</Icon></span>
